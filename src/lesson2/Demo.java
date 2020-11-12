ae4fccfe7addf67893f47f53539f64a000bd821d
Crabislav <alex.kopnin2@gmail.com>
2020-11-12 23:47:30 +0200

    change structure

--- src/lesson2/Demo.java
+++ src/lesson2/Demo.java
@@ -1,15 +1,19 @@
 package lesson2;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         //java
             //1. create class with fields +
-            //2. create obj with some values
-            //3. using it
+            //2. create obj with some values +
+            //3. using it +
+
+        Order order = new Order(1, "tool1", 150, new Date(), new Date());
 
         //db
-            //1. create table with columns -
-            //2. insert some data to the table
-            //3. using it
+            //1. create table with columns +
+            //2. insert some data to the table +
+            //3. using it +
     }
 }
