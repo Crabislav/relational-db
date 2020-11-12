fa99b6884c57980955061fbf3b7ad8173e4d2cd1
Crabislav <alex.kopnin2@gmail.com>
2020-11-12 23:47:59 +0200

    add lesson3

---
+++ src/sql/homework/lesson3/INSERT_INTO_PRODUCT.sql
@@ -0,0 +1,29 @@
+INSERT INTO PRODUCT
+VALUES (1, 'product1', 'manufacturer', 10);
+
+INSERT INTO PRODUCT
+VALUES (2, 'product2', 'manufacturer', 20);
+
+INSERT INTO PRODUCT
+VALUES (3, 'product3', 'manufacturer', 30);
+
+INSERT INTO PRODUCT
+VALUES (4, 'product4', 'manufacturer', 40);
+
+INSERT INTO PRODUCT
+VALUES (5, 'product5', 'manufacturer', 50);
+
+INSERT INTO PRODUCT
+VALUES (6, 'product6', 'manufacturer', 60);
+
+INSERT INTO PRODUCT
+VALUES (7, 'product7', 'manufacturer', 70);
+
+INSERT INTO PRODUCT
+VALUES (8, 'product8', 'manufacturer', 80);
+
+INSERT INTO PRODUCT
+VALUES (9, 'product9', 'manufacturer', 90);
+
+INSERT INTO PRODUCT
+VALUES (10, 'product10', 'manufacturer', 100);
